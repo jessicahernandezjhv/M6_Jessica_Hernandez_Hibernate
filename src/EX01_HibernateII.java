71d8c4bdeb3c77d417c8693edbad164e60f40e9b
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2019-01-29 19:54:35 +0100

    Insertar fila

--- src/EX01_HibernateII.java
+++ src/EX01_HibernateII.java
@@ -2,7 +2,7 @@ import primeroo.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-public class Main {
+public class EX01_HibernateII {
 	public static void main(String[] args){
 		//En primer lugar se obtiene la sesión creada por el Singleton
 		
@@ -13,9 +13,9 @@ public class Main {
 		System.out.println("Inserto una fila en depart");
 		//Creamos un nuevo objeto Depart y damos valor a sus atributos
 		Depart dep = new Depart();
-		dep.setDeptNo((byte) 62);
-		dep.setDnombre("MARKETING");
-		dep.setLoc("GUADALAJARA");
+		dep.setDeptNo((byte) 45);
+		dep.setDnombre("LIMPIEZA");
+		dep.setLoc("MARSELLA");
 		//Guardamos en la base de datos y comprometemos la información
 		session.save(dep);
 		tx.commit();
---
+++ src/EX02_HibernateII.java
@@ -0,0 +1,4 @@
+
+public class EX02_HibernateII {
+
+}
